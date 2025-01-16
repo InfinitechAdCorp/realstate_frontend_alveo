9409914426f71fdbc3cfa8f118d510a37236d1ff
Infinitech-Lyca <infinitech.lycanobleza@gmail.com>
2025-01-16 16:02:19 +0800

    modify modals

--- app/pages/buildings/[slug]/page.js
+++ app/pages/buildings/[slug]/page.js
@@ -288,10 +288,11 @@ export default function BlogPost ({ params }) {
           </div>
 
           {isOpen && (
-            <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50'>
-              <div className='bg-white p-6 rounded-lg shadow-lg w-11/12 sm:w-4/5 md:w-3/4 lg:w-1/2 flex flex-col md:flex-row gap-6 relative'>
+            <div className='fixed inset-0 flex items-center justify-center bg-customBlue bg-opacity-60 z-50'>
+              <div className='bg-white p-6 shadow-lg w-11/12 sm:w-4/5 md:w-3/4 lg:w-1/2 flex flex-col md:flex-row gap-6 relative'>
                 <button
-                  className='absolute top-4 right-4 p-2 bg-red-500 text-white rounded-full hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-opacity-75'
+                  className='absolute top-0 right-4 p-2 text-2xl text-black rounded-full
+                  '
                   onClick={togglePopup}
                 >
                   &times;
@@ -326,73 +327,75 @@ export default function BlogPost ({ params }) {
                   </p>
                 </div>
 
-                <div className='w-full md:w-1/2'>
-                  <h2 className='text-xl font-semibold mb-4'>
+                <div className='w-full md:w-1/2 mt-2'>
+                  <h2 className='text-2xl font-semibold text-customBlue mb-4'>
                     {popupType === 'Request Viewing'
                       ? 'Schedule Appointment'
                       : 'Submit Inquiry'}
                   </h2>
                   <form
-                    className='grid grid-cols-1 md:grid-cols-2 gap-4'
+                    className='grid gap-4'
                     onSubmit={e => {
                       e.preventDefault()
                       submitForm()
                     }}
                   >
-                    <div>
-                      <label className='block text-sm font-medium text-gray-700'>
-                        Name
-                      </label>
-                      <input
-                        type='text'
-                        name='name'
-                        value={formData.name}
-                        onChange={handleInputChange}
-                        className='mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500'
-                        placeholder='Your Name'
-                      />
+                    <div className='grid grid-cols-1 sm:grid-cols-2 gap-4'>
+                      <div>
+                        <label className='block text-sm font-medium text-customBlue'>
+                          Name
+                        </label>
+                        <input
+                          type='text'
+                          name='name'
+                          value={formData.name}
+                          onChange={handleInputChange}
+                          className='mt-1 block w-full p-2 border border-cyan-700 focus:ring-blue-500 focus:border-blue-500'
+                          placeholder='Your Name'
+                        />
+                      </div>
+                      <div>
+                        <label className='block text-sm font-medium text-customBlue'>
+                          Phone Number
+                        </label>
+                        <input
+                          type='tel'
+                          name='phone'
+                          value={formData.phone}
+                          onChange={handleInputChange}
+                          className='mt-1 block w-full p-2 border border-cyan-700 focus:ring-customBlue focus:border-blue-500'
+                          placeholder='Your Phone Number'
+                        />
+                      </div>
+                      <div>
+                        <label className='block text-sm font-medium text-customBlue'>
+                          Email
+                        </label>
+                        <input
+                          type='email'
+                          name='email'
+                          value={formData.email}
+                          onChange={handleInputChange}
+                          className='mt-1 block w-full p-2 border border-cyan-700 focus:ring-customBlue focus:border-blue-500'
+                          placeholder='Your Email'
+                        />
+                      </div>
+                      <div>
+                        <label className='block text-sm font-medium text-customBlue'>
+                          {popupType === 'Request Viewing'
+                            ? 'Appointment Date & Time'
+                            : 'Preferred Contact Date & Time'}
+                        </label>
+                        <input
+                          type='datetime-local'
+                          name='appointmentDate'
+                          value={formData.appointmentDate}
+                          onChange={handleInputChange}
+                          className='mt-1 block w-full p-2 border border-cyan-700 focus:ring-customBlue focus:border-blue-500'
+                        />
+                      </div>
                     </div>
                     <div>
-                      <label className='block text-sm font-medium text-gray-700'>
-                        Phone Number
-                      </label>
-                      <input
-                        type='tel'
-                        name='phone'
-                        value={formData.phone}
-                        onChange={handleInputChange}
-                        className='mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500'
-                        placeholder='Your Phone Number'
-                      />
-                    </div>
-                    <div>
-                      <label className='block text-sm font-medium text-gray-700'>
-                        Email
-                      </label>
-                      <input
-                        type='email'
-                        name='email'
-                        value={formData.email}
-                        onChange={handleInputChange}
-                        className='mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500'
-                        placeholder='Your Email'
-                      />
-                    </div>
-                    <div>
-                      <label className='block text-sm font-medium text-gray-700'>
-                        {popupType === 'Request Viewing'
-                          ? 'Appointment Date & Time'
-                          : 'Preferred Contact Date & Time'}
-                      </label>
-                      <input
-                        type='datetime-local'
-                        name='appointmentDate'
-                        value={formData.appointmentDate}
-                        onChange={handleInputChange}
-                        className='mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500'
-                      />
-                    </div>
-                    <div className='flex flex-col gap-4'>
                       <div>
                         <label className='block text-sm font-medium text-gray-700'>
                           Message
@@ -401,14 +404,16 @@ export default function BlogPost ({ params }) {
                           name='message'
                           value={formData.message}
                           onChange={handleInputChange}
-                          className='mt-1 p-1 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 resize-none h-20 w-full'
+                          className='mt-1 p-1 border border-gray-300 rounded-md shadow-sm 
+                          focus:ring-blue-500 focus:border-blue-500 resize-none h-20 w-full'
                           placeholder='Type your message here'
                         />
                       </div>
 
                       <button
                         type='submit'
-                        className='px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75'
+                        className='px-4 py-2 bg-customBlue text-white 
+                        hover:bg-cyan-700 items-center'
                       >
                         {popupType === 'Request Viewing'
                           ? 'Confirm Appointment'
